12d24a25fd1b64e302b7c5520725713a58287a6f
Clara Hu <mhu@health.ucsd.edu>
2023-02-28 12:30:52 -0800

    add import argparse

--- cellmaps_hierarchyeval/make_hidefOnt_utils.py
+++ cellmaps_hierarchyeval/make_hidefOnt_utils.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 from collections import defaultdict
+import argparse
 
 ### One step making ontology from the hidef Hierarchy 
 ## make a long table of (parent, child, edgetype)
