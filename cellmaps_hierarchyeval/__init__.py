6264f868dc281c7942538858910af421212e0ec0
dzoanka <joanna.lenkiewicz@gmail.com>
2023-11-09 10:57:43 -0800

    Bumped version

--- cellmaps_hierarchyeval/__init__.py
+++ cellmaps_hierarchyeval/__init__.py
@@ -2,8 +2,8 @@
 
 """Top-level package for cellmaps_hierarchyeval."""
 
-__author__ = 'Ideker Lab CM4AI team'
+__author__ = 'Ideker Lab Cell Maps team'
 __email__ = 'tools@cm4ai.org'
-__version__ = '0.1.0a1'
+__version__ = '0.1.0a2'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_hierarchyeval'
 __description__ = 'A tool to evaluate hierarchies with known resources'
