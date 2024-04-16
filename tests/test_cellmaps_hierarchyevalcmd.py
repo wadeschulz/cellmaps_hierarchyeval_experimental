f452c3ebc07370c786098a4b0762e8a31dcfbaf4
Chris Churas <churas.camera@gmail.com>
2024-04-16 09:32:25 -0700

    added tests for get_ollama_geneset_agents

--- tests/test_cellmaps_hierarchyevalcmd.py
+++ tests/test_cellmaps_hierarchyevalcmd.py
@@ -9,6 +9,9 @@ import shutil
 
 import unittest
 from cellmaps_hierarchyeval import cellmaps_hierarchyevalcmd
+from cellmaps_hierarchyeval.analysis import FakeGeneSetAgent
+from cellmaps_hierarchyeval.analysis import OllamaRestServiceGenesetAgent
+from cellmaps_hierarchyeval.analysis import OllamaCommandLineGeneSetAgent
 
 
 class TestCellmapshierarchyeval(unittest.TestCase):
@@ -55,3 +58,32 @@ class TestCellmapshierarchyeval(unittest.TestCase):
             self.assertEqual(res, 2)
         finally:
             shutil.rmtree(temp_dir)
+
+    def test_get_ollama_geneset_agents_no_prompts(self):
+
+        res = cellmaps_hierarchyevalcmd.get_ollama_geneset_agents(ollama_prompts=None)
+        self.assertIsNone(res)
+
+    def test_get_ollama_geneset_agents_service_prompts(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            prompt_file = os.path.join(temp_dir, 'myprompt.txt')
+            with open(prompt_file, 'w') as f:
+                f.write('my prompt')
+            o_prompts = ['fake', 'modela,' + prompt_file,
+                         'modelb,a prompt']
+            res = cellmaps_hierarchyevalcmd.get_ollama_geneset_agents(ollama='http://foo',
+                                                                      ollama_prompts=o_prompts)
+            self.assertEqual(3, len(res))
+            self.assertTrue(isinstance(res[0], FakeGeneSetAgent))
+            self.assertTrue(isinstance(res[1], OllamaRestServiceGenesetAgent))
+            self.assertEqual(res[1].get_prompt(), 'my prompt')
+            self.assertTrue(isinstance(res[2], OllamaRestServiceGenesetAgent))
+            self.assertEqual(res[2].get_prompt(), 'a prompt')
+
+
+        finally:
+            shutil.rmtree(temp_dir)
+
+
+
