174af24d3affc95036eb396bc09d48192d1507fe
Chris Churas <churas.camera@gmail.com>
2024-04-16 09:50:52 -0700

    bumped version and changed author

--- cellmaps_hierarchyeval/__init__.py
+++ cellmaps_hierarchyeval/__init__.py
@@ -2,8 +2,8 @@
 
 """Top-level package for cellmaps_hierarchyeval."""
 
-__author__ = 'Ideker Lab Cell Maps team'
+__author__ = 'Cell Maps team'
 __email__ = 'tools@cm4ai.org'
-__version__ = '0.1.0a7'
+__version__ = '0.1.0a8'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_hierarchyeval'
 __description__ = 'A tool to evaluate hierarchies with known resources'
