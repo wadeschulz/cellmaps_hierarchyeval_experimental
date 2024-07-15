aafc094ed23842d28f69f27795f413a3bb81d52c
dzoanka <joanna.lenkiewicz@gmail.com>
2024-07-15 13:07:11 -0700

    Refactor and update methods, add docstrings

--- cellmaps_hierarchyeval/perturb.py
+++ cellmaps_hierarchyeval/perturb.py
@@ -1,12 +1,11 @@
-
-import os
 import logging
 import math
 import pandas as pd
-from scipy.stats import ranksums
 from scipy import stats
 import cellmaps_utils.music_utils as music_utils
+from ndex2 import constants
 
+from cellmaps_hierarchyeval.exceptions import CellmapshierarchyevalError
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +15,7 @@ class PerturbSeqAnalysis(object):
     Contains utilities to compare Perturbation data
     against hierarchy passed in via constructor
     """
+
     def __init__(self, hierarchy, hierarchy_parent):
         """
         Constructor
@@ -42,11 +42,13 @@ class PerturbSeqAnalysis(object):
         :type hier_system_node_id: int
         :param perturbseq_df:
         :type perturbseq_df: :py:class:`pandas.DataFrame`
+        :param num_perturb_seq:
+        :type num_perturb_seq: int
         :return: heat map table
         :rtype: :py:class:`pandas.DataFrame`
         """
-        # TODO: load hierarchy and get genes for that system. put into variable assembly_genes
-        assembly_genes = []
+        node_values = self._hierarchy.get_node(hier_system_node_id)
+        assembly_genes = node_values[constants.ASPECT_VALUES]['CD_MemberList'].split(' ')
         cluster_genes_in_perturb = [x for x in assembly_genes if x in perturbseq_df.index.values]
 
         # from notebook but changed to match these variables
@@ -58,42 +60,57 @@ class PerturbSeqAnalysis(object):
 
     def get_root_node_pair_similarities(self):
         """
-        Gets root node pair similarities
-        # Todo: explain this better
+        Calculates similarity scores between gene pairs in the root node of a hierarchy. Genes in the same community
+        linked to the root node are marked with a similarity of 0, indicating they are directly related,
+        while all other pairs are set to 1, suggesting no direct relation.
 
-        :return:
+        :return: A DataFrame with genes as both rows and columns, populated with similarity scores.
         :rtype: :py:class:`pandas.DataFrame`
         """
+        root_node = None
+        genes = []
         # get pairs only in root node
         for nodeid, node in self._hierarchy.get_nodes().items():
-            if node['v']['HCX::isRoot'] == True:
+            if node[constants.ASPECT_VALUES]['HCX::isRoot']:
                 root_node = nodeid
-                genes = node['v']['CD_MemberList'].split(' ')
+                genes = node[constants.ASPECT_VALUES]['CD_MemberList'].split(' ')
                 logger.debug('Number of genes: ' + str(len(genes)))
 
+        if root_node is None:
+            logger.error('No root node detected!')
+            raise CellmapshierarchyevalError('No root node detected!')
+
+        # Get nodes that are directly connected to root node
         communities_connected_to_root = []
         for edgeid, edge in self._hierarchy_parent.get_edges().items():
-            if edge['s'] == root_node:
-                communities_connected_to_root.append(edge['t'])
-        # print(len(communities_connected_to_root))
+            if edge[constants.EDGE_SOURCE] == root_node:
+                communities_connected_to_root.append(edge[constants.EDGE_TARGET])
 
+        # Assign similarity scores
         root_pairs = pd.DataFrame(1, index=genes, columns=genes)
         for community in communities_connected_to_root:
             community_node = self._hierarchy.get_node(community)
-            community_genes = community_node['v']['CD_MemberList'].split(' ')
+            community_genes = community_node[constants.ASPECT_VALUES]['CD_MemberList'].split(' ')
             root_pairs.loc[community_genes, community_genes] = 0
         return root_pairs
 
-    def get_root_overlapping_pair_simliarities(self, root_pairs, perturbseq_df):
+    @staticmethod
+    def get_root_overlapping_pair_similarities(root_pairs, perturbseq_df):
         """
         Get similarity scores from **perturbseq_df** that match genes attached to the root
         node of the hierarchy
 
-        :param root_pairs:
-        :type root_pairs: # TODO: what is this
+        :param root_pairs: A DataFrame representing similarity scores between all genes in the root node,
+                            where genes within the same community connected to the root have a score of 0,
+                            indicating direct relation, and all other pairs have a score of 1,
+                            indicating no direct relation.
+        :type root_pairs: :py:class:`pandas.DataFrame`
         :param perturbseq_df:
         :type perturbseq_df: :py:class:`pandas.DataFrame`
-        :return: (:py:func:`numpy.array`, None) # TODO: replace None with actual type and explanation
+        :return: A tuple containing:
+             - A DataFrame of scaled cosine similarity scores for overlapping genes in communities direct to root
+                and Perturb-seq data.
+             - A DataFrame of root-associated similarity scores, filtered to only include overlapping genes.
         :rtype: tuple
         """
         overlap_genes = list(set(root_pairs.index.values).intersection(set(perturbseq_df.index.values)))
@@ -103,15 +120,21 @@ class PerturbSeqAnalysis(object):
 
         return functional_data_similarity, overlap_root_pairs
 
-    def get_data_simliarity_root(self, functional_data_similarity, overlap_root_pairs):
+    @staticmethod
+    def get_data_simliarity_root(functional_data_similarity, overlap_root_pairs):
         """
-        # TODO: explain what this is doing
+        Extracts and returns a list of functional similarity scores for gene pairs that are not in the same community,
+            based on a filtered upper triangle extraction of the similarity matrix (ensures that only unique,
+            non-redundant gene pair comparisons are considered).
 
-        :param functional_data_similarity:
-        :type functional_data_similarity: # TODO: what is this
-        :param overlap_root_pairs:
-        :type overlap_root_pairs: # TODO: what is this
-        :return:
+        :param functional_data_similarity: A DataFrame of scaled cosine similarity scores for overlapping genes in
+                                            communities direct to root and Perturb-seq data.
+        :type functional_data_similarity: :py:class:`pandas.DataFrame`
+        :param overlap_root_pairs: A DataFrame of root-associated similarity scores, filtered to only include
+                                    overlapping genes. A score of 0 indicates a direct relation (same community)
+                                    and scores greater than 0 indicate no direct relation
+        :type overlap_root_pairs: :py:class:`pandas.DataFrame`
+        :return: A list of non-NaN similarity scores for gene pairs that are not directly related.
         :rtype: list
         """
         root_mask = overlap_root_pairs > 0
@@ -122,16 +145,27 @@ class PerturbSeqAnalysis(object):
 
     def get_cluster_similarity(self, functional_data_similarity, hier_system_node_id):
         """
-        # TODO: explain what this is doing
+        Retrieves the upper triangle similarity scores for genes within a specific cluster of a hierarchy.
+        The scores are extracted from a DataFrame that contains scaled cosine similarity scores for genes that overlap
+        between communities direct to root and Perturb-seq data.
 
-        :param functional_data_similarity:
-        :type functional_data_similarity: # TODO: what is this
-        :param hier_system_node_id:
+        :param functional_data_similarity: A DataFrame of scaled cosine similarity scores for overlapping genes in
+                                            communities direct to root and Perturb-seq data.
+        :type functional_data_similarity: :py:class:`pandas.DataFrame`
+        :param hier_system_node_id: The identifier for a specific node within a hierarchy.
         :type hier_system_node_id: int
-        :return:
+        :return: An array of similarity scores from the upper triangle portion of the matrix for the specified cluster.
         :rtype: :py:func:`numpy.array`
         """
-        cluster_genes = [] # TODO: set cluster_genes to list of genes in assembly with id hier_system_node_id
+        # Retrieve node information and extract gene members from the specified cluster.
+        node_values = self._hierarchy.get_node(hier_system_node_id)
+        cluster_genes = node_values[constants.ASPECT_VALUES]['CD_MemberList'].split(' ')
+
+        # Filter genes that are present in the functional data similarity matrix.
         cluster_genes_in_functional_data = [x for x in cluster_genes if x in functional_data_similarity.index.values]
-        return music_utils.upper_tri_values(functional_data_similarity.loc[cluster_genes_in_functional_data,
-                                                                           cluster_genes_in_functional_data])
+
+        # Extract the relevant portion of the similarity matrix and return upper triangle values.
+        similarity_submatrix = functional_data_similarity.loc[
+            cluster_genes_in_functional_data, cluster_genes_in_functional_data]
+
+        return music_utils.upper_tri_values(similarity_submatrix)
