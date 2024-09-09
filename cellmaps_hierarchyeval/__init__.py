9a8db1e3838f67ea8aad886d853cc814b67be545
dzoanka <joanna.lenkiewicz@gmail.com>
2024-09-09 11:53:16 -0700

    Drop alpha

--- cellmaps_hierarchyeval/__init__.py
+++ cellmaps_hierarchyeval/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = 'Cell Maps team'
 __email__ = 'tools@cm4ai.org'
-__version__ = '0.1.0a8'
+__version__ = '0.1.0'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_hierarchyeval'
 __description__ = 'A tool to evaluate hierarchies with known resources'
